b80815953a6867d96ea540d528d9bdd9eb02072c
PrasadDevops42 <prasadbulli@gmail.com>
2019-11-17 16:57:12 +0530

    lists added

---
+++ Variables/lists/main.tf
@@ -0,0 +1,10 @@
+variable "sample" {
+  type    = "list"
+  default = ["prasad", "bhagya","papa"]
+}
+
+resource "null_resource" "sample" {
+  provisioner "local-exec" {
+    command = "echo ${var.sample[3]}"
+  }
+}
